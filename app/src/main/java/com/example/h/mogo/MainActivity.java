7d684a6e86d18224f85064030f4f2afd9df67eb3
Hongjun Choi <hong_jun_choi@brown.edu>
2015-07-26 00:03:12 -0400

    camera start 1.0

--- app/src/main/java/com/example/h/mogo/MainActivity.java
+++ app/src/main/java/com/example/h/mogo/MainActivity.java
@@ -1,37 +1,41 @@
 package com.mobvcasting.videocapturewithpreview;
 
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.media.CamcorderProfile;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import com.example.h.mogo.CameraPreview;
 import com.example.h.mogo.R;
-import com.parse.Parse;
-import com.parse.ParseFile;
-import com.parse.ParseObject;
 
 import java.io.File;
-import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class MainActivity extends Activity implements OnClickListener, SurfaceHolder.Callback {
+public class MainActivity extends Activity  {
 
     public static final String LOGTAG = "VIDEOCAPTURE";
-
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
     private MediaRecorder recorder;
     private SurfaceHolder holder;
     private CamcorderProfile camcorderProfile;
     private Camera camera;
-
+    private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
+    private Uri fileUri;
+    private Camera mCamera;
+    private CameraPreview mPreview;
     boolean recording = false;
     boolean usecamera = true;
     boolean previewRunning = false;
@@ -39,181 +43,115 @@ public class MainActivity extends Activity implements OnClickListener, SurfaceHo
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         // Enable Local Datastore.
-        Parse.enableLocalDatastore(this);
+//        Parse.enableLocalDatastore(this);
+//
+//        Parse.initialize(this, "Twc5KQkxtq0yenh2uHBp3GVwfqW48kwKIgLThZvM", "vLlrTC1DrowiyZJtzURRuSUpI64dOFvoBt1AqRIC");
+//
+//
+//        byte[] data = "MOGO!".getBytes();
+//        ParseFile file = new ParseFile("first.txt", data);
+//        file.saveInBackground();
+//
+//        ParseObject obj = new ParseObject("FileUpload");
+//
+//        obj.put("user", file);
+//
+//        obj.saveInBackground();
+        // create Intent to take a picture and return control to the calling application
+        //create new Intent
 
-        Parse.initialize(this, "Twc5KQkxtq0yenh2uHBp3GVwfqW48kwKIgLThZvM", "vLlrTC1DrowiyZJtzURRuSUpI64dOFvoBt1AqRIC");
-
-
-        byte[] data = "MOGO!".getBytes();
-        ParseFile file = new ParseFile("first.txt", data);
-        file.saveInBackground();
-
-        ParseObject obj = new ParseObject("FileUpload");
-
-        obj.put("user", file);
-
-        obj.saveInBackground();
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
-        camcorderProfile = CamcorderProfile.get(CamcorderProfile.QUALITY_LOW);
-
-        setContentView(R.layout.activity_main);
-
-        SurfaceView cameraView = (SurfaceView) findViewById(R.id.CameraView);
-        holder = cameraView.getHolder();
-        holder.addCallback(this);
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
-        cameraView.setClickable(true);
-        cameraView.setOnClickListener(this);
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_VIDEO);  // create a file to save the video
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);  // set the image file name
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1); // set the video image quality to high
+        // start the Video Capture Intent
+        startActivityForResult(intent, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
     }
 
-    private void prepareRecorder() {
-        recorder = new MediaRecorder();
-        recorder.setPreviewDisplay(holder.getSurface());
 
-        if (usecamera) {
-            camera.unlock();
-            recorder.setCamera(camera);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // Video captured and saved to fileUri specified in the Intent
+                Toast.makeText(this, "Video saved to:\n" +
+                        data.getData(), Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                // User cancelled the video capture
+            } else {
+                // Video capture failed, advise user
+            }
         }
+    }
 
-        recorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
-        recorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
-
-        recorder.setProfile(camcorderProfile);
-
-        // This is all very sloppy
-        if (camcorderProfile.fileFormat == MediaRecorder.OutputFormat.THREE_GPP) {
-            try {
-                File newFile = File.createTempFile("videocapture", ".3gp", Environment.getExternalStorageDirectory());
-                recorder.setOutputFile(newFile.getAbsolutePath());
-            } catch (IOException e) {
-                Log.v(LOGTAG,"Couldn't create file");
-                e.printStackTrace();
-                finish();
-            }
-        } else if (camcorderProfile.fileFormat == MediaRecorder.OutputFormat.MPEG_4) {
-            try {
-                File newFile = File.createTempFile("videocapture", ".mp4", Environment.getExternalStorageDirectory());
-                recorder.setOutputFile(newFile.getAbsolutePath());
-            } catch (IOException e) {
-                Log.v(LOGTAG,"Couldn't create file");
-                e.printStackTrace();
-                finish();
-            }
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
         } else {
-            try {
-                File newFile = File.createTempFile("videocapture", ".mp4", Environment.getExternalStorageDirectory());
-                recorder.setOutputFile(newFile.getAbsolutePath());
-            } catch (IOException e) {
-                Log.v(LOGTAG,"Couldn't create file");
-                e.printStackTrace();
-                finish();
-            }
-
+            // no camera on this device
+            return false;
         }
-        //recorder.setMaxDuration(50000); // 50 seconds
-        //recorder.setMaxFileSize(5000000); // Approximately 5 megabytes
+    }
 
+    /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
         try {
-            recorder.prepare();
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-            finish();
-        } catch (IOException e) {
-            e.printStackTrace();
-            finish();
+            c = Camera.open(); // attempt to get a Camera instance
         }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 
-    public void onClick(View v) {
-        if (recording) {
-            recorder.stop();
-            if (usecamera) {
-                try {
-                    camera.reconnect();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
             }
-            // recorder.release();
-            recording = false;
-            Log.v(LOGTAG, "Recording Stopped");
-            // Let's prepareRecorder so we can record again
-            prepareRecorder();
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
         } else {
-            recording = true;
-            recorder.start();
-            Log.v(LOGTAG, "Recording Started");
-        }
-    }
-
-    public void surfaceCreated(SurfaceHolder holder) {
-        Log.v(LOGTAG, "surfaceCreated");
-
-        if (usecamera) {
-            camera = Camera.open();
-
-            try {
-                camera.setPreviewDisplay(holder);
-                camera.startPreview();
-                previewRunning = true;
-            }
-            catch (IOException e) {
-                Log.e(LOGTAG,e.getMessage());
-                e.printStackTrace();
-            }
+            return null;
         }
 
+        return mediaFile;
     }
 
 
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        Log.v(LOGTAG, "surfaceChanged");
-
-        if (!recording && usecamera) {
-            if (previewRunning){
-                camera.stopPreview();
-            }
-
-            try {
-                Camera.Parameters p = camera.getParameters();
-
-                p.setPreviewSize(camcorderProfile.videoFrameWidth, camcorderProfile.videoFrameHeight);
-                p.setPreviewFrameRate(camcorderProfile.videoFrameRate);
-
-                camera.setParameters(p);
-
-                camera.setPreviewDisplay(holder);
-                camera.startPreview();
-                previewRunning = true;
-            }
-            catch (IOException e) {
-                Log.e(LOGTAG,e.getMessage());
-                e.printStackTrace();
-            }
-
-            prepareRecorder();
-        }
-    }
 
 
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        Log.v(LOGTAG, "surfaceDestroyed");
-        if (recording) {
-            recorder.stop();
-            recording = false;
-        }
-        recorder.release();
-        if (usecamera) {
-            previewRunning = false;
-            //camera.lock();
-            camera.release();
-        }
-        finish();
-    }
+
+
 }
