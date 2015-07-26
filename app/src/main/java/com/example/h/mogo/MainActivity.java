cb6d2a38ea98cacf2a6c80b2d571632cd9578077
sean <hyun_chang_song@brown.edu>
2015-07-26 02:55:16 -0400

    video upload / parse

--- app/src/main/java/com/example/h/mogo/MainActivity.java
+++ app/src/main/java/com/example/h/mogo/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.hardware.Camera;
 import android.media.CamcorderProfile;
 import android.media.MediaRecorder;
@@ -14,12 +15,23 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.h.mogo.CameraPreview;
 import com.example.h.mogo.R;
+import com.parse.Parse;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -33,6 +45,7 @@ public class MainActivity extends Activity  {
     private CamcorderProfile camcorderProfile;
     private Camera camera;
     private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
+    static final int REQUEST_VIDEO_CAPTURE = 1;
     private Uri fileUri;
     private Camera mCamera;
     private CameraPreview mPreview;
@@ -45,9 +58,11 @@ public class MainActivity extends Activity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Enable Local Datastore.
-//        Parse.enableLocalDatastore(this);
-//
-//        Parse.initialize(this, "Twc5KQkxtq0yenh2uHBp3GVwfqW48kwKIgLThZvM", "vLlrTC1DrowiyZJtzURRuSUpI64dOFvoBt1AqRIC");
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "Twc5KQkxtq0yenh2uHBp3GVwfqW48kwKIgLThZvM", "vLlrTC1DrowiyZJtzURRuSUpI64dOFvoBt1AqRIC");
+
+
 //
 //
 //        byte[] data = "MOGO!".getBytes();
@@ -61,7 +76,7 @@ public class MainActivity extends Activity  {
 //        obj.saveInBackground();
         // create Intent to take a picture and return control to the calling application
         //create new Intent
-
+/*
         Intent intent = new Intent("android.media.action.VIDEO_CAMERA");
 
         String path = Environment.getExternalStorageDirectory().getAbsolutePath();
@@ -74,13 +89,107 @@ public class MainActivity extends Activity  {
         intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
         intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1); // set the video image quality to high
         // start the Video Capture Intent
-        startActivityForResult(intent, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
+        startActivityForResult(intent, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);*/
+
+        dispatchTakeVideoIntent();
+    }
+    private void dispatchTakeVideoIntent() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
     }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        System.out.println("====================== START OF ON ACTRESULT  =======================");
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            // mVideoView.setVideoURI(videoUri);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
+            try {
+                Uri androidUri = data.getData();
+                System.out.print(androidUri);
+                Uri auri = data.getData();
+                String path = auri.getPath();
+
+               // File filex = new File(new java.net.URI(URLEncoder.encode(auri.toString(), "UTF-8")));
+                File filex = new File(getRealPathFromURI(this, auri));
+
+                byte[] byteX = getBytesFromFile(filex);
+                ParseFile file = new ParseFile("firstV.mp4", byteX);
+                file.saveInBackground();
+                TextView tv = (TextView)findViewById(R.id.txtview1);
+                tv.setText(androidUri.toString()+"??????"+filex.toString()+"////" +byteX.toString());
+
+                ParseObject obj = new ParseObject("VideoUpload");
+
+                 obj.put("firstUpload", file);
+
+                  obj.saveInBackground();
+
+                /*FileInputStream fis = new FileInputStream(file);
+                byte[] byteData = new byte[(int)file.length()];
+                DataInputStream dataIs = new DataInputStream(fis);*/
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.print("?????????????????????///11111111111111111111111/dfadsifc");
+            }
+        }
+    }
+
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            System.out.print(cursor.getString(column_index));
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    public static byte[] getBytesFromFile(File file) throws IOException {
+        InputStream is = new FileInputStream(file);
+
+        // Get the size of the file
+        long length = file.length();
+
+        // You cannot create an array using a long type.
+        // It needs to be an int type.
+        // Before converting to an int type, check
+        // to ensure that file is not larger than Integer.MAX_VALUE.
+        if (length > Integer.MAX_VALUE) {
+            // File is too large
+        }
+
+        // Create the byte array to hold the data
+        byte[] bytes = new byte[(int)length];
+
+        // Read in the bytes
+        int offset = 0;
+        int numRead = 0;
+        while (offset < bytes.length
+                && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
+            offset += numRead;
+        }
+
+        // Ensure all the bytes have been read in
+        if (offset < bytes.length) {
+            throw new IOException("Could not completely read file "+file.getName());
+        }
+
+        // Close the input stream and return bytes
+        is.close();
+        return bytes;
+    }
+/*
         if (requestCode == CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 // Video captured and saved to fileUri specified in the Intent
@@ -92,7 +201,7 @@ public class MainActivity extends Activity  {
                 // Video capture failed, advise user
             }
         }
-    }
+    }*/
 
     /** Check if this device has a camera */
     private boolean checkCameraHardware(Context context) {
